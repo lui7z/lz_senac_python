0502b1e29760f67ab49ece81ab563a6d65b3f4ee
Lui7z <luizfm727@gmail.com>
2025-04-01 21:52:54 -0300

    commit 2

---
+++ a1/t3.py
@@ -0,0 +1,4 @@
+num1 = int(input("Type the first number: "))
+num2 = int(input("Type the second number: "))
+
+print("The sum is equal to:", num1 + num2)
---
+++ a1/t4.py
@@ -0,0 +1,8 @@
+num1 = float(input("Type your first note: "))
+num2 = float(input("Type your second note: "))
+num3 = float(input("Type your third note: "))
+num4 = float(input("Type your fourth note: "))
+
+average = (num1 + num2 + num3 + num4) / 4
+
+print("The average is equal to:", average)
