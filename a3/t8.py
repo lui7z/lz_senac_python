0341bbfdeaab0a45ddc29fe1b21ae39c8ae7e137
Lui7z <luizfm727@gmail.com>
2025-04-03 19:39:42 -0300

    commit a2

---
+++ a3/t8.py
@@ -0,0 +1,7 @@
+money = float(input("Type how much you gain per hour: "))
+hours = float(input("Type how many hours you work per month: "))
+
+total = money * hours
+total = total * 30
+
+print("You gain a total of:", total, "per month!")
