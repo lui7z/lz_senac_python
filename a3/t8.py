85128b649cd3df2f9711c31e55793b2098fdba6c
Lui7z <luizfm727@gmail.com>
2025-04-03 20:00:32 -0300

    commit a3

--- a3/t8.py
+++ a3/t8.py
@@ -2,6 +2,5 @@ money = float(input("Type how much you gain per hour: "))
 hours = float(input("Type how many hours you work per month: "))
 
 total = money * hours
-total = total * 30
 
 print("You gain a total of:", total, "per month!")
---
+++ a3/t9.py
@@ -0,0 +1,4 @@
+far = float(input("Type a number in Fahrenheit degrees: "))
+
+celsius = 5 * ((far-32) / 9)
+print("Your Fahrenheit degrees in Celsius are equal to:", celsius)
