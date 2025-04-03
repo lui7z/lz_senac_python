6f2acba4d1e9034b6ccff2a9dfe24a7354b56a43
Lui7z <luizfm727@gmail.com>
2025-04-02 21:28:39 -0300

    commit a2

---
+++ a2/t6.py
@@ -0,0 +1,4 @@
+raio = float(input("Type the value of your ray: "))
+
+area = (raio * raio) * 3.14
+print ("The Area of the ray that you typed is: ", area)
---
+++ a2/t7.py
@@ -0,0 +1,5 @@
+lateral = float(input("Type a number of the lateral of your square: "))
+
+area = lateral * lateral
+double = area * 2
+print("The area of the square is equal to:", area, "and the double is equal to:", double)
