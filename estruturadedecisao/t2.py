d2c6ae7bc055b839c12411514da92640452ece31
Lui7z <luizfm727@gmail.com>
2025-04-09 19:47:40 -0300

    commit on branch

--- estruturadedecisao/t2.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Type a number: "))
-
-if n1 > 0:
-    print("The number:", n1,"is positive!")
-elif n1 < 0:
-    print("The number:", n1,"is negative!")
-else:
-    print("The number that you typed was Zero!")
--- estruturadedecisao/t3.py
+++
@@ -1,8 +0,0 @@
-letter = input("Type a Letter: ")
-
-if letter.upper() == "F":
-    print("F - Feminino")
-elif letter.upper() == "M":
-    print("M - Masculino")
-else:
-    print("Sexo Inválido")
