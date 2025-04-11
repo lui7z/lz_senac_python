c3ff4eac79954c0f63a79b34f04119de7869dbe2
Lui7z <luizfm727@gmail.com>
2025-04-11 20:27:55 -0300

    commit on branch

--- estruturaderepeticao/t1.py
+++
@@ -1,7 +0,0 @@
-note = int(input("Type a note between 0 and 10: "))
-
-while note < 0 or note > 10:
-    print("invalid")
-    note = int(input("Type a valid note: "))
-
-print("ok")
--- estruturaderepeticao/t2.py
+++
@@ -1,8 +0,0 @@
-user = input("Type your User: ")
-password = input("Type your Password: ")
-
-while user == password:
-    user = input("Invalid Password! \nPlease Type a valid user and password! \nType your User: ")
-    password = input("Type your Password: ")
-
-print("Login Ok!")
